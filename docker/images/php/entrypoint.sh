20591e5880f89169dc55aa589b353ddc59cec641
splogamurugan <splogamurugan@gmail.com>
2018-08-26 01:56:43 -0400

    added all ansible files

---
+++ docker/images/php/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+nohup php -S localhost:8080 -t /var/www > phpd.log 2>&1 &
+
+/usr/sbin/sshd -D
+
---
+++ entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+nohup php -S localhost:8080 -t /var/www > phpd.log 2>&1 &
+
+
